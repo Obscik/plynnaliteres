cb03b30c21a60de6c7f3500b2e78ecbc3ffaaa94
Chomik Bodzio <38500332+Obscik@users.noreply.github.com>
2025-04-14 10:32:09 +0200

    Remove unused components from index page and comment out header link

--- components/layouts/Header.vue
+++ components/layouts/Header.vue
@@ -39,7 +39,7 @@ const { title, github } = useAppConfig()
           <div
             class="flex-col w-full h-auto overflow-hidden rounded-lg bg-background md:overflow-visible md:rounded-none md:relative md:flex md:flex-row"
           >
-            <a
+            <!-- <a
               href="/"
               :title="title"
               class="inline-flex items-center w-auto h-16 px-4 text-xl font-black leading-none text-gray-900 dark:text-gray-100 md:hidden"
@@ -54,16 +54,11 @@ const { title, github } = useAppConfig()
                 >
               </span>
               <span class="mx-2">{{ title }}</span>
-            </a>
+            </a> -->
             <div class="w-auto mx-4" />
             <div
               class="flex flex-col items-start justify-end w-full pt-4 md:items-center md:flex-row md:py-0"
             >
-              <a
-                class="w-full px-6 py-2 mr-0 text-gray-700 cursor-pointer dark:text-gray-300 md:px-3 md:mr-2 lg:mr-3 md:w-auto"
-                href="/dashboard"
-                :title="`${title} Dashboard`"
-              >{{ $t('dashboard.title') }}</a>
               <a
                 :href="github"
                 target="_blank"
@@ -74,8 +69,6 @@ const { title, github } = useAppConfig()
                   class="w-5 h-5 mr-1"
                 />
                 GitHub</a>
-
-              <SwitchLanguage />
 
               <SwitchTheme />
             </div>
